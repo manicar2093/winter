63ee97bb4486642b361f48cfbfc74de91a8bdc40
Manuel Carbajal <manicar2093@gmail.com>
2025-02-01 17:50:35 -0600

    feat: add common used structs

---
+++ commonreq/structs.go
@@ -0,0 +1,18 @@
+package commonreq
+
+import "github.com/google/uuid"
+
+type (
+	GetById struct {
+		Id int `header:"id" param:"id" form:"id" query:"id" json:"id" validate:"required"`
+	}
+
+	GetByIdUUID struct {
+		Id uuid.UUID `header:"id" param:"id" form:"id" query:"id" json:"id" validate:"required_uuid"`
+	}
+
+	PageData struct {
+		PageNumber int `header:"page_number" param:"page_number" form:"page_number" query:"page_number" json:"page_number"`
+		PageSize   int `header:"page_size" param:"page_size" form:"page_size" query:"page_size" json:"page_size"`
+	}
+)
